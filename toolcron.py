7897f58ffb41e7de90edd460bb49a5eba02be76b
UserDT28 <131569925+UserDT28@users.noreply.github.com>
2023-04-23 10:54:19 +0700

    Update toolcron.py

--- toolcron.py
+++ toolcron.py
@@ -4,6 +4,7 @@ import time
 os.system('clear')
 cpr=("\033[39m•  \033[1;37mYoutuber : \033[33mD T I Z E N 2\n\033[39m•  Zalo : \033[36m0338743623\n\033[39m•  \033[31mRANDOM : \033[35mRANDOM\033[36mDTIZEN2\033[37m.SITE")
 print(cpr)
+print('\033[31m P/s : Mỗi lần Cron Chỉ Được 3 Link Nên Anh Em Cron Nhiều Hơn Thì Đa Tab Ra Nhé ! ' )
 momo=input('\033[1;37mLink Cron Momo \n=>\033[33m')
 bank= input('\033[1;37mLink Cron Bank\n=>\033[33m')
 tsr= input('\033[1;37mLink Cron TSR\n\033[33m')
