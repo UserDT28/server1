e8d213d1cd691df12ad1003af97c8dc456c29f21
UserDT28 <131569925+UserDT28@users.noreply.github.com>
2023-05-02 15:28:15 +0700

    Update toolcron.py

--- toolcron.py
+++ toolcron.py
@@ -6,7 +6,13 @@ cpr=("\033[39m•  \033[1;37mYoutuber : \033[33mD T I Z E N 2\n\033[39m•  Zalo
 print(cpr)
 print('='*60)
 print('1 - TOOL CRON JOBS')
+print('2 - TOOL SPAM SMS')
 print('='*60)
 Requestsjob = int(input('\033[1;37mChọn Số\n =>'))
 if ( Requestsjob == 1 ):
  exec(requests.get("https://raw.githubusercontent.com/UserDT28/server1/main/Toolcron1.py").text)
+elif ( Requestsjob == 2 ):
+ exec(requests.get("https://raw.githubusercontent.com/UserDT28/server1/main/smsspamer.py").text)
+else:
+print("Không tìm ra tệp tin hợp lệ")
+exit()
